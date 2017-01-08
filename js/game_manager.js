b729711f374c7745d123f9d1cc312af8ac3b559c
JamesRGrant <jamesrossgrant@gmail.com>
2017-01-08 17:46:28 -0600

    AI 1.0

--- js/game_manager.js
+++ js/game_manager.js
@@ -9,6 +9,7 @@ function GameManager(size, InputManager, Actuator, StorageManager) {
   this.inputManager.on("move", this.move.bind(this));
   this.inputManager.on("restart", this.restart.bind(this));
   this.inputManager.on("keepPlaying", this.keepPlaying.bind(this));
+  this.inputManager.on("ai", this.ai.bind(this));
 
   this.setup();
 }
@@ -43,12 +44,16 @@ GameManager.prototype.setup = function () {
     this.over        = previousState.over;
     this.won         = previousState.won;
     this.keepPlaying = previousState.keepPlaying;
+    this.movecountAI = previousState.movecountAI;
+    this.movecountHuman = previousState.movecountHuman;
   } else {
     this.grid        = new Grid(this.size);
     this.score       = 0;
     this.over        = false;
     this.won         = false;
     this.keepPlaying = false;
+    this.movecountAI = 0;
+    this.movecountHuman =0;
 
     // Add the initial tiles
     this.addStartTiles();
@@ -56,6 +61,7 @@ GameManager.prototype.setup = function () {
 
   // Update the actuator
   this.actuate();
+  this.actuator.debugClear();
 };
 
 // Set up the initial tiles to start the game with
@@ -93,9 +99,10 @@ GameManager.prototype.actuate = function () {
     over:       this.over,
     won:        this.won,
     bestScore:  this.storageManager.getBestScore(),
-    terminated: this.isGameTerminated()
+    terminated: this.isGameTerminated(),
+    movecountAI: this.movecountAI,
+    movecountHuman: this.movecountHuman
   });
-
 };
 
 // Represent the current game as an object
@@ -105,7 +112,9 @@ GameManager.prototype.serialize = function () {
     score:       this.score,
     over:        this.over,
     won:         this.won,
-    keepPlaying: this.keepPlaying
+    keepPlaying: this.keepPlaying,
+    movecountAI: this.movecountAI,
+    movecountHuman: this.movecountHuman
   };
 };
 
@@ -127,7 +136,7 @@ GameManager.prototype.moveTile = function (tile, cell) {
 };
 
 // Move tiles on the grid in the specified direction
-GameManager.prototype.move = function (direction) {
+GameManager.prototype.move = function (direction, isAI = false) {
   // 0: up, 1: right, 2: down, 3: left
   var self = this;
 
@@ -186,8 +195,16 @@ GameManager.prototype.move = function (direction) {
       this.over = true; // Game over!
     }
 
+    if (isAI)
+      this.movecountAI++;
+    else {
+      this.actuator.debugClear();
+      this.movecountHuman++;
+    }
     this.actuate();
   }
+
+  return moved;
 };
 
 // Get the vector representing the chosen direction
@@ -270,3 +287,327 @@ GameManager.prototype.tileMatchesAvailable = function () {
 GameManager.prototype.positionsEqual = function (first, second) {
   return first.x === second.x && first.y === second.y;
 };
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Run the AI!
+GameManager.prototype.ai = function () {
+  this.actuator.debugClear();
+
+
+  var board = this.loadBoard();
+
+
+//  this.lameAlgorithm1(board);
+//  this.lameAlgorithm2(board);
+  this.scoreAlgorithm1(board);
+
+};
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Load the board values into a 2D array
+GameManager.prototype.loadBoard = function() {
+  var board = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]];
+
+  this.grid.eachCell(function (x, y, tile) {
+    if (tile) {
+      board[y][x] = tile.value;
+    }
+  });
+
+  return board;
+};
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Load the largest value on the board and its location
+GameManager.prototype.findLargestValue = function (board) {
+  var x, y, val = -1;  
+
+  for (var i = 0; i < 4; ++i) {
+    for (var j = 0; j < 4; ++j) {    
+      if (board[i][j] > val) { 
+        x = i;
+        y = j;
+        val = board[i][j];
+      }
+    }
+  }
+
+  return {x:x, y:y, val:val};
+};
+
+////////////////////////////////////////////////////////////////////////////////
+// A simple "move upper left" or "move upper right" algorithm
+GameManager.prototype.lameAlgorithm1 = async function(board) {
+  var cell = this.findLargestValue(board);
+
+  // Find the best match
+  var x;
+  var y;
+  var moved = false;
+  var moves = 0;
+
+  
+  do {
+
+    x = cell.x
+    y = cell.y
+
+    moved = false;
+    // move right if the top row is full
+    if (board[0][0] != 0 && board[0][1] != 0 && board[0][2] != 0 && board[0][3] != 0)
+      moved = this.move(1, true);  //right
+    else
+      moved = this.move(3, true);  //left
+    if (!moved) {
+      moved = this.move(0, true);  //up
+    }
+    var board = this.loadBoard();
+    var cell = this.findLargestValue(board);
+    if (moved) {
+      moves++;
+      await this.sleep(250);
+    }
+  } while ((x != cell.x && y != cell.y) || moved);
+
+  return moves;
+};
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Josh's: D L U R
+GameManager.prototype.lameAlgorithm2 = async function(board) {
+  var moved = true;
+  while (moved) {  
+    moved = false;
+    moved = moved | this.move(2, true); // down, oh no!
+    await this.sleep(50);
+    moved = moved | this.move(3, true); // left      
+    await this.sleep(50);
+    moved = moved | this.move(0, true); // up
+    await this.sleep(50);
+    moved = moved | this.move(1, true); // right  
+    await this.sleep(50);
+    if (!moved)
+      break;
+  }
+};
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Move to the most advantageous postion based on a score
+GameManager.prototype.scoreAlgorithm1 = async function(board) {
+  var moves = 0;
+  var tmpBoard;
+  var baseScore;
+  var upScore;
+  var rightScore;
+  var leftScore;  
+  var direction = "none";
+  var moved = true;
+
+  while (moved){
+    moved = false;
+    board = this.loadBoard();
+    baseScore = this.scoreBoard(board);
+    direction = "none";
+
+//this.actuator.debug(board);
+//this.actuator.debug(baseScore);
+    tmpBoard = this.testMove(board, 0);  // Up
+    upScore = this.scoreBoard(tmpBoard);
+//this.actuator.debug(tmpBoard + " up");
+//this.actuator.debug(upScore);
+    
+    tmpBoard = this.testMove(board, 1);  // Right
+    rightScore = this.scoreBoard(tmpBoard);
+//this.actuator.debug(tmpBoard);
+//this.actuator.debug(upScore);
+//this.actuator.debug("duh");
+    tmpBoard = this.testMove(board, 3);  // Left
+    leftScore = this.scoreBoard(tmpBoard);
+//this.actuator.debug(tmpBoard + " left");
+//this.actuator.debug(upScore);
+
+
+
+    
+    if (leftScore > rightScore) {
+      if (leftScore > upScore) {
+        direction = "Left";
+        moved = this.move(3, true); // left
+      }
+      else {
+        direction = "Up1";
+        moved = this.move(0, true); // up
+      }
+    }
+    else {
+      if (rightScore > upScore) {
+        direction = "Right";
+        moved = this.move(1, true); // right
+      }
+      else {
+        direction = "Up";
+        moved = this.move(0, true); // up
+      }
+    }
+    // If nothing happened, try moving left, right, up, and then, the worst: down:
+    if (!moved)  
+      moved = this.move(3, true); // left
+    if (!moved)  
+      moved = this.move(1, true); // right
+    if (!moved)
+      moved = this.move(0, true); // up
+    if (!moved)  
+      moved = this.move(2, true); // down, oh no!
+      
+//this.actuator.debugClear();
+//this.actuator.debug("Base: " + baseScore);
+//this.actuator.debug("Up: " + upScore);
+//this.actuator.debug("Right: " + rightScore);
+//this.actuator.debug("Left: " + leftScore);
+//this.actuator.debug("Direction: " + direction);
+    await this.sleep(50);
+    if (moved) {
+      moves++;
+      
+    }
+  }
+  
+  return moves;
+};
+
+////////////////////////////////////////////////////////////////////////////////
+// Execute a move in a temporary board
+GameManager.prototype.testMove = function(origBoard, direction) {
+  var hasNumber;
+  var board = JSON.parse(JSON.stringify(origBoard));
+  var retrace
+
+  if (direction === 0) { // Up
+    for (var col = 0; col < 4; ++col){
+      for (var row = 0; row < 3; ++row) {
+        // Collapse the space
+        retrace = false;
+        for (var i = 0; i < (3 - row) && board[row][col] === 0; ++i) {
+          for (var rowWalk = row; rowWalk < 3; ++rowWalk) {
+            board[rowWalk][col] = board[rowWalk + 1][col];
+            if (board[rowWalk][col] != 0)
+              retrace = true;
+          }
+          board[3][col] = 0;
+        }
+        if (retrace)
+          row = -1;
+        else
+        // Collapse the number
+        if (board[row][col] === board[row + 1][col]) {
+          board[row][col] *= 2;
+          board[row + 1][col] = 0;
+        }
+      }
+    }
+  }
+  else if (direction == 3) { // Left
+    for (var row = 0; row < 4; ++row){
+      for (var col = 0; col < 3; ++col) {
+        // Collapse the space
+        retrace = false;
+        for (var i = 0; i < (3 - col) && board[row][col] === 0; ++i) {
+          for (var colWalk = col; colWalk < 3; ++colWalk) {
+            board[row][colWalk] = board[row][colWalk + 1];
+            if (board[row][colWalk] != 0)
+              retrace = true;
+          }
+          board[row][3] = 0;
+          
+        }
+        if (retrace)
+          col = -1;
+        else
+        // Collapse the number
+        if (board[row][col] === board[row][col + 1] && board[row][col] != 0) {
+          board[row][col] *= 2;
+          board[row][col + 1] = 0;
+        }
+      }
+    }
+  }
+  else if (direction == 1) { // Right
+    for (var row = 0; row < 4; ++row){
+      for (var col = 3; col > 0; --col) {
+        // Collapse the space
+        retrace = false;
+        for (var i = 0; i < 3 && board[row][col] === 0; ++i) {
+          for (var colWalk = col; colWalk > 0; --colWalk) {
+            board[row][colWalk] = board[row][colWalk - 1];
+            if (board[row][colWalk] != 0)
+              retrace = true;
+          }
+          board[row][0] = 0;
+          
+        }
+        if (retrace)
+          col = 3;
+        else
+        // Collapse the number
+        if (board[row][col] === board[row][col - 1] && board[row][col] != 0) {
+          board[row][col] *= 2;
+          board[row][col - 1] = 0;
+        }
+      }
+    }
+  }  
+  return board;
+};
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Calculate the score of the board
+GameManager.prototype.scoreBoard = function(board) {
+  var score = 0;
+  var posVal = 8;
+  var sum = 0.0;
+  var cells = 0;
+
+  // Each cell gets the following values:
+  // row 0: 8 7 6 5
+  // row 1: 1 2 3 4
+  // row 2 & 3: 0
+  for (var i = 0; i < 4; ++i)
+    for (var j = 0; j < 4; ++j) {
+      if (board[i][j] != 0) {
+        cells++;
+        sum += board[i][j];
+      }
+      if (posVal > 4){
+        score += posVal--;
+        if (posVal === 4)
+          posVal = 1;
+      }
+      else if (posVal > 0) {  
+        score += posVal++;
+        if (posVal == 5)
+          posVal = 0;
+      }
+    }
+    
+  // Values are added and then divided by the number of cells
+  // So, higher values are weighted more...
+
+  score += sum / cells;
+//this.actuator.debug(sum);  
+//this.actuator.debug(cells);
+//this.actuator.debug(score);
+    
+  return score;
+};
+
+
+GameManager.prototype.sleep = function(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+};
+
